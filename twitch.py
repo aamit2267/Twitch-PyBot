04f9f29e4395259a6cb526bf0abfbba1dfd62a1a
Amit Agarwal <60168762+aamit2267@users.noreply.github.com>
2021-01-17 09:05:31 +0530

    Create twitch.py

---
+++ twitch.py
@@ -0,0 +1,80 @@
+'''
+Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+'''
+
+import sys
+import irc.bot
+import requests
+
+class TwitchBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, username, client_id, token, channel):
+        self.client_id = client_id
+        self.token = token
+        self.channel = '#' + channel
+        url = 'https://api.twitch.tv/amitagarwal2412/users?login=' + channel
+        headers = {'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        r = requests.get(url, headers=headers).json()
+        self.channel_id = r['users'][0]['_id']
+        server = 'irc.chat.twitch.tv'
+        port = 6667
+        print 'Connecting to ' + server + ' on port ' + str(port) + '...'
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+token)], username, username)
+        
+
+    def on_welcome(self, ct, e):
+        print 'Joining ' + self.channel
+        ct.cap('REQ', ':twitch.tv/membership')
+        ct.cap('REQ', ':twitch.tv/tags')
+        ct.cap('REQ', ':twitch.tv/commands')
+        ct.join(self.channel)
+
+    def on_pubmsg(self, c, e):
+        if e.arguments[0][:1] == '!':
+            cmd = e.arguments[0].split(' ')[0][1:]
+            print 'Received command: ' + cmd
+            self.do_command(e, cmd)
+        return
+
+    def do_command(self, e, cmd):
+        c = self.connection
+        if cmd == "game":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, r['display_name'] + ' is currently playing ' + r['game'])
+        elif cmd == "title":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, r['display_name'] + ' channel title is currently ' + r['status'])
+
+        elif cmd == "raffle":
+            message = "This is an example bot, replace this text with your raffle text."
+            c.privmsg(self.channel, message)
+        elif cmd == "schedule":
+            message = "This is an example bot, replace this text with your schedule text."            
+            c.privmsg(self.channel, message)
+        else:
+            c.privmsg(self.channel, "Did not understand command: " + cmd)
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: twitchbot <username> <client id> <token> <channel>")
+        sys.exit(1)
+
+    username  = sys.argv[1]
+    client_id = sys.argv[2]
+    token     = sys.argv[3]
+    channel   = sys.argv[4]
+
+    bot = TwitchBot(username, client_id, token, channel)
+    bot.start()
+
+if __name__ == "__main__":
+    main()
